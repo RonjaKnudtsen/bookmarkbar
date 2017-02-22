7ab3558b36ee3ee9061181f978256a9e63192c01
Ronja Knudtsen <ronjark@gmail.com>
2017-02-22 09:28:00 +0100

    Upload

--- script.js
+++ script.js
@@ -1,201 +1,150 @@
-
-
-console.log("Test");
+console.log("Executing: Future Group application test");
 //From drag.html
 'use strict';
 
 window.onload = function() {
   const STANDARD_CLICK = 0;
-
   let drag_target_ = null;
   let drag_delta_ = {'x': 0, 'y': 0};
-  var list = null;
-  var prevDrag = 0;
-  var direction = null;
 
-  init();
+  let directionLeft = false;
+  let isRemoving = false; //To prevent looping.   
+  let dragX = null;
+  let dragY = null;
+  var prevClientX = 0;
 
-  function init (item){
-    list = document.getElementById('bookmark-bar');
-    list.items = document.getElementsByTagName('li');
+  let bookmarkBar = document.getElementById('bookmark-bar');
+  let bookmarks = bookmarkBar.getElementsByTagName('li');
 
-    //Get the list items, give them startPos and endPos.
-    for (var i = 0, len = list.items.length; i<len; i++ ) {
+  //Start by running rePosition so we can give the bookmarks correct startposition. 
+  rePosition();
 
-      var item = list.items[i];
-
-      if (i == 0) {
-        item.prev = null;
-        item.startPos = 0;
-
-        item.endPos = item.clientWidth;
-        item.next = list.items[i+1];
-
-      } else if( i == list.items.length){
-        item.next = null;
-        item.prev = list.items[i-1];
-
-        item.startPos = item.prev.endPos;
-        item.endPos = item.startPos + item.clientWidth;
-      }
-      else {         
-        item.prev = list.items[i-1];
-        item.next = list.items[i+1];
-
-        item.startPos = item.prev.endPos;
-        item.endPos = item.startPos + item.clientWidth;
-      }
-
-      item.old_startPos = item.startPos;
-      item.old_endPos = item.endPos;
-      console.log("init",item.old_startPos );
-
-      item.style.left = item.startPos + "px";
-
-      item.addEventListener('mousedown', mousedown_);
+  function rePosition(doAnimation){
+    for (var i = 0, len = bookmarks.length; i<len; i++ ) {
+  		if( i != 0){
+  			//Get previous elments startpos and clientWidth, use this to reposition "this". 
+  			bookmarks[i].style.left =  parseInt(bookmarks[i].previousElementSibling.style.left) + bookmarks[i].previousElementSibling.clientWidth + "px";
+  		} else {
+  			bookmarks[i].style.left = 0 + "px";			
+  		}
+  		bookmarks[i].style.top = 0 + "px";  
     }
-
   }
 
-  var mousedown_ = function(event) {
-    if (event.button == STANDARD_CLICK) {
+  let animateMove = function (element){
+  	element.classList.add("move");
+  	 element.addEventListener("transitionend", function(event) {
+  	  event.target.classList.remove("move");
+  	}, false);
+  }
 
-     // console.log(event.target.parentNode);
+  let animateFade = function (element){
+  	element.classList.add("fade");
+  }
+
+  let mousedown_ = function(event) {  	
+  	//When clicking the boxes we are clicking on h2 (since h2 is the only object without position relative), so we need to get its parentNodes' parentNode. 
+    if (event.target.parentNode.parentNode.id == 'bookmark-bar' && event.button == STANDARD_CLICK) {
       drag_target_ = event.target.parentNode;
-      let style = drag_target_.style;
 
-      drag_target_.id = "active";
-      prevDrag = event.clientX;
+      let style = drag_target_.style;
       drag_delta_.x = event.clientX - parseInt(style.left);
       drag_delta_.y = event.clientY - parseInt(style.top);
-
       document.addEventListener('mousemove', mousemove_);
       document.addEventListener('mouseup', mouseup_);
-
       event.preventDefault();
     }
   };
 
-  var mousemove_ = function(event) {
-    
-    const alpha = 0.3;
-   //console.log("target",drag_target_.next);
-
-    let dragX = event.clientX - drag_delta_.x;
-    let dragY = event.clientY - drag_delta_.y;
-
-   
+  let mousemove_ = function(event) {
+  	//This is used to compare pos.
+  	dragX = event.clientX - drag_delta_.x;
+    dragY = event.clientY - drag_delta_.y;
 
     drag_target_.style.left = `${dragX}px`;
     drag_target_.style.top = `${dragY}px`;
 
-    drag_target_.style.zIndex = 1000;
+    if(dragY > 50 || dragY < -50 && !isRemoving){
+      isRemoving = true;
 
+      animateFade(drag_target_);
+      removeDrag_targetBookmark();
 
-    if((prevDrag - event.clientX) > 0 && drag_target_.prev){
-      direction = "left";
-
-     var center = ((1-alpha)*drag_target_.prev.endPos + (alpha)*drag_target_.prev.startPos);
-
-      if(center > dragX ){
-        
-        moveRight();
-        
-      }
-
-    } else if(drag_target_.next){
-
-      var center = (alpha*drag_target_.next.endPos + (1-alpha)*drag_target_.next.startPos);
-
-      if(center < dragX + drag_target_.clientWidth){         
-        moveLeft();            
-      }
-      
-      
-      //Going left
-      
-      
-
+    } else if((prevClientX - event.clientX) > 0 && drag_target_.previousElementSibling){
+    	directionLeft = true;
+    	checkSwap(drag_target_.previousElementSibling);
+    	
+    } else if((prevClientX - event.clientX) < 0  && drag_target_.nextElementSibling){
+    	directionLeft = false;
+    	checkSwap(drag_target_.nextElementSibling);    	
     }
 
-    prevDrag = event.clientX;
-
-
-
+    prevClientX = event.clientX;
   };
 
-  var moveRight = function(){
-    //Only do this once.   
-    //The previous items startpos needs to move foreward by "clientWidthPX"
-    drag_target_.startPos = drag_target_.prev.startPos;
-    drag_target_.prev.startPos += drag_target_.clientWidth;
-    animate(drag_target_.prev);
-    drag_target_.prev.style.left = drag_target_.prev.startPos + "px";
-    
+  let removeDrag_targetBookmark = function(){
 
-    //Now relocate prev and next items.     
-    if(drag_target_.prev.prev){
-      drag_target_.prev.prev.next = drag_target_;
-    }
-   drag_target_.next.prev = drag_target_.prev;
-    drag_target_.prev = drag_target_.prev.prev;
-    //data_target.prev.prev.next = drag_target_;
-       
-  
-  }
-  var moveLeft = function(){
-    //The next items startpos needs to move backwards by "clientWidthPX"
+    //Wait for fade to complete.
+    drag_target_.addEventListener("transitionend", function(event) {
 
-    if(drag_target_.next == null){
-      console.log("end of the line");
-    } else {
+        //Check if exists, to prevent uncaught reference error. 
+        if(drag_target_){
 
-      drag_target_.next.startPos -= drag_target_.clientWidth;
-      drag_target_.next.endPos = drag_target_.next.startPos + drag_target_.next.clientWidth;
-      animate(drag_target_.next);
-      drag_target_.next.style.left = drag_target_.next.startPos + "px";
+          //Add animation to all elements that will collapse (everyone to the right). 
+          let temp = drag_target_.nextElementSibling;
+          while(temp){
+            animateMove(temp);
+            temp = temp.nextElementSibling;
+          }
 
-      //The new startPos for drag_target is the oldPos of the item we just moved. 
-      drag_target_.startPos = drag_target_.next.endPos;
-      //Set the dragged target as prev to nexts next.  
-      if(drag_target_.next.next){
-        console.log("not nulL",drag_target_.next.next.prev)
-        drag_target_.next.next.prev = drag_target_;
-      }
-      
-      drag_target_.next = drag_target_.next.next;       
-    }
-  
+          bookmarkBar.removeChild(drag_target_);
+          rePosition(true);
+          drag_target_ = null;
+          event.target.classList.remove("fade");
+          isRemoving = false;
+        }
+      }, false);
   }
 
-  var mouseup_ = function(event) {
+  //Check if we need to swap. 
+  function checkSwap(drop_target_){
+
+	 let startPos = parseInt(drop_target_.style.left);
+   let endPos = drop_target_.clientWidth + startPos;
+   let center = (startPos + endPos )/ 2;
+
+   //different behavior based on direction. 
+
+    if (dragX < center && directionLeft ) {
+    	animateMove(drop_target_);
+    	//new pos = drop_target.pos + width of dragged item. 
+    	drop_target_.style.left = parseInt(drop_target_.style.left) + drag_target_.clientWidth + "px";
+    	bookmarkBar.insertBefore(drag_target_, drop_target_);
+    } 
+    else if (dragX > center && directionLeft == false) {
+    	animateMove(drop_target_);
+    	//new pos = drop_target.pos - width of dragged item. 
+    	drop_target_.style.left = parseInt(drop_target_.style.left) - drag_target_.clientWidth + "px";
+    	//Insert before next (in order to insert after). 
+    	bookmarkBar.insertBefore(drag_target_, drop_target_.nextElementSibling);
+    }
+
+  }
+
+  let mouseup_ = function(event) {
     document.removeEventListener('mousemove', mousemove_);
     document.removeEventListener('mouseup', mouseup_);
 
-    drag_target_.style.left = `${drag_target_.startPos}px`;
-    animate(drag_target_);
-    drag_target_.style.zIndex = 1;
-    drag_target_ = null;
+    //if we are removing an object we should wait for fade. 
+    if(!isRemoving){      
+      animateMove(drag_target_);
+      rePosition(true);
+      drag_target_ = null;
+    }
+  	
 
   };
 
   document.addEventListener('mousedown', mousedown_);
-};
 
-let animate = function(element){
-    element.classList.add("move");
-     element.addEventListener("transitionend", function(event) {
-      event.target.classList.remove("move");
-    }, false);
 }
-
-//From animate.html
-let set_new_randow_position = function() {
-  let ele = document.querySelector('#target');
-  let style = ele.style;
-  style.left =
-      `${Math.trunc(Math.random() * (window.innerWidth - ele.clientWidth))}px`;
-  style.top =
-      `${Math.trunc(Math.random() * (window.innerHeight - ele.clientHeight))}px`;
-};
-
